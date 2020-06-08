246d4fff63ac2db2afbd46ee06fa870a17f8ad82
Ryan Rosiak <ryan11291129@gmail.com>
2020-06-08 15:22:59 -0400

    Added a test program and created the repository

---
+++ test/main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+    int num;
+
+    printf("Hello World!\n");
+    printf("Please enter a number: ");
+    scanf("%d", &num);
+    printf("Your number that you entered was... %d", num);
+
+    return 0;
+}
